8d4ee75ed7c690ebca1be84914cd6d3451b80996
986256696@qq.com <27430526+zhangyidegithub@users.noreply.github.com>
2019-04-19 16:13:59 +0800

    title logo 更改

--- ant-design-jeecg-vue/src/components/tools/UserMenu.vue
+++ ant-design-jeecg-vue/src/components/tools/UserMenu.vue
@@ -9,31 +9,31 @@
         <a-avatar class="avatar" size="small" :src="getAvatar()"/>
         <span v-if="isDesktop()">欢迎您，{{ nickname() }}</span>
       </span>
-      <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
-        <!--<a-menu-item key="0">-->
-          <!--<router-link :to="{ name: 'account-center' }">-->
-            <!--<a-icon type="user"/>-->
-            <!--<span>个人中心</span>-->
-          <!--</router-link>-->
+      <!--<a-menu slot="overlay" class="user-dropdown-menu-wrapper">-->
+        <!--&lt;!&ndash;<a-menu-item key="0">&ndash;&gt;-->
+          <!--&lt;!&ndash;<router-link :to="{ name: 'account-center' }">&ndash;&gt;-->
+            <!--&lt;!&ndash;<a-icon type="user"/>&ndash;&gt;-->
+            <!--&lt;!&ndash;<span>个人中心</span>&ndash;&gt;-->
+          <!--&lt;!&ndash;</router-link>&ndash;&gt;-->
+        <!--&lt;!&ndash;</a-menu-item>&ndash;&gt;-->
+        <!--&lt;!&ndash;<a-menu-item key="1">&ndash;&gt;-->
+          <!--&lt;!&ndash;<router-link :to="{ name: 'account-settings' }">&ndash;&gt;-->
+            <!--&lt;!&ndash;<a-icon type="setting"/>&ndash;&gt;-->
+            <!--&lt;!&ndash;<span>账户设置</span>&ndash;&gt;-->
+          <!--&lt;!&ndash;</router-link>&ndash;&gt;-->
+        <!--&lt;!&ndash;</a-menu-item>&ndash;&gt;-->
+       <!--&lt;!&ndash; <a-menu-item key="2" disabled>-->
+          <!--<a-icon type="setting"/>-->
+          <!--<span>测试</span>-->
         <!--</a-menu-item>-->
-        <a-menu-item key="1">
-          <router-link :to="{ name: 'account-settings' }">
-            <a-icon type="setting"/>
-            <span>账户设置</span>
-          </router-link>
-        </a-menu-item>
-       <!-- <a-menu-item key="2" disabled>
-          <a-icon type="setting"/>
-          <span>测试</span>
-        </a-menu-item>
-        <a-menu-divider/>
-        <a-menu-item key="3">
-          <a href="javascript:;" @click="handleLogout">
-            <a-icon type="logout"/>
-            <span>退出登录</span>
-          </a>
-        </a-menu-item>-->
-      </a-menu>
+        <!--<a-menu-divider/>-->
+        <!--<a-menu-item key="3">-->
+          <!--<a href="javascript:;" @click="handleLogout">-->
+            <!--<a-icon type="logout"/>-->
+            <!--<span>退出登录</span>-->
+          <!--</a>-->
+        <!--</a-menu-item>&ndash;&gt;-->
+      <!--</a-menu>-->
     </a-dropdown>
     <span class="action">
       <a class="logout_title" href="javascript:;" @click="handleLogout">
